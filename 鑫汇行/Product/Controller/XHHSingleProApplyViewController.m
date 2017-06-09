bec3951fa60141dfe71c15680badd9569a833be3
WeaponChan <Lhkh@LHKHdeMacBook-Pro.local>
2017-06-09 16:22:27 +0800

    提交申请格式验证修改

--- 鑫汇行/Product/Controller/XHHSingleProApplyViewController.m
+++ 鑫汇行/Product/Controller/XHHSingleProApplyViewController.m
@@ -504,7 +504,6 @@ minimumLineSpacingForSectionAtIndex:(NSInteger)section
     modifyparams[@"user_id"] = @(user_id.integerValue);
     modifyparams[@"pic"] = imagestr;
     NSString *url = [NSString stringWithFormat:@"%@/app.php/WebService?action=1016&key=%@&phone=%@",XHHBaseUrl,user_key,user];
-    NSLog(@"----params=%@",modifyparams);
     [LhkhHttpsManager requestWithURLString:url parameters:modifyparams type:2 success:^(id responseObject) {
         NSLog(@"-----myinfo=%@",responseObject);
         if ([responseObject[@"status"] isEqualToString:@"1"]) {
@@ -570,7 +569,48 @@ minimumLineSpacingForSectionAtIndex:(NSInteger)section
 
 //申请
 -(void)applyAct{
-    NSLog(@"user_name===%@",user_name.text);
+    NSString *regex = @"[a-zA-Z. ]*";
+    NSPredicate *Engpredicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
+    NSString *match = @"(^[\u4e00-\u9fa5]+$)";
+    NSPredicate *Zhpredicate = [NSPredicate predicateWithFormat:@"SELF matches %@", match];
+    NSString *num = @"[0-9]";
+    NSPredicate *Numgpredicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", num];
+    NSString *namestr = user_name.text;
+    if (user_name.text == nil || [user_name.text isEqualToString:@""]) {
+        [MBProgressHUD show:@"姓名不能为空" view:self.view];
+        return;
+    }
+    if (user_name.text.length < 2 || user_name.text.length >30) {
+        [MBProgressHUD show:@"姓名长度不能低于2且不能大于30，请重新修改" view:self.view];
+        return;
+    }
+    if ([Engpredicate evaluateWithObject:namestr] == NO && [Zhpredicate evaluateWithObject:namestr] == NO) {
+        [MBProgressHUD show:@"姓名只能是全英文或者全汉字,请修改!" view:self.view];
+        return;
+    }
+    if (icard.text == nil || [icard.text isEqualToString:@""]) {
+        [MBProgressHUD show:@"身份证号码不能为空" view:self.view];
+        return;
+    }
+    
+    if ([self judgeIdentityStringValid:icard.text] == NO) {
+        [MBProgressHUD show:@"身份证号码格式不对" view:self.view];
+        return;
+    }
+    
+    if (tel_phone.text.length !=11 || tel_phone.text == nil || [tel_phone.text isEqualToString:@""]) {
+        [MBProgressHUD show:@"手机号格式错误，请重新填写" view:self.view];
+        return;
+    }
+    if ([Numgpredicate evaluateWithObject:tel_phone.text] == YES) {
+        [MBProgressHUD show:@"手机号只能是数字，请重新填写" view:self.view];
+        return;
+    }
+    if (order_money.text == nil || [order_money.text isEqualToString:@""] || [Numgpredicate evaluateWithObject:order_money.text] == YES) {
+        [MBProgressHUD show:@"申请金额格式有误，请正确填写" view:self.view];
+        return;
+    }
+    
     NSMutableDictionary *applyparams = [NSMutableDictionary  dictionary];
     NSString *user_id =  [[NSUserDefaults standardUserDefaults]objectForKey:@"USER_ID"];
     NSString *user =  [[NSUserDefaults standardUserDefaults]objectForKey:@"USER"];
@@ -604,6 +644,77 @@ minimumLineSpacingForSectionAtIndex:(NSInteger)section
         [MBProgressHUD show:str view:self.view];
     }];
 
+}
+
+#pragma mark 判断身份证号是否合法
+- (BOOL)judgeIdentityStringValid:(NSString *)identityString {
+    NSLog(@"identityString===%@",identityString);
+
+    if ([identityString length] < 15 ||[identityString length] > 18) {
+        return NO;
+    }
+ 
+    //加权因子
+    int R[] ={7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2 };
+    //校验码
+    unsigned char sChecker[11]={'1','0','X', '9', '8', '7', '6', '5', '4', '3', '2'};
+    
+    //将15位身份证号转换成18位
+    
+    NSMutableString *mString = [NSMutableString stringWithString:identityString];
+    if ([identityString length] == 15) {
+        
+        
+        [mString insertString:@"19" atIndex:6];
+        
+        long p = 0;
+        const char *pid = [mString UTF8String];
+        for (int i=0; i<=16; i++)
+        {
+            p += (pid[i]-48) * R[i];
+        }
+        
+        int o = p%11;
+        NSString *string_content = [NSString stringWithFormat:@"%c",sChecker[o]];
+        [mString insertString:string_content atIndex:[mString length]];
+        identityString = mString;
+        
+    }
+    
+    
+    // 正则表达式判断基本 身份证号是否满足格式
+    NSString *regex2 = @"^(^[1-9]\\d{7}((0\\d)|(1[0-2]))(([0|1|2]\\d)|3[0-1])\\d{3}$)|(^[1-9]\\d{5}[1-9]\\d{3}((0\\d)|(1[0-2]))(([0|1|2]\\d)|3[0-1])((\\d{4})|\\d{3}[Xx])$)$";
+    NSPredicate *identityStringPredicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",regex2];
+    //如果通过该验证，说明身份证格式正确，但准确性还需计算
+    if(![identityStringPredicate evaluateWithObject:identityString]) return NO;
+    //** 开始进行校验 *//
+    //将前17位加权因子保存在数组里
+    NSArray *idCardWiArray = @[@"7", @"9", @"10", @"5", @"8", @"4", @"2", @"1", @"6", @"3", @"7", @"9", @"10", @"5", @"8", @"4", @"2"];
+    //这是除以11后，可能产生的11位余数、验证码，也保存成数组
+    NSArray *idCardYArray = @[@"1", @"0", @"10", @"9", @"8", @"7", @"6", @"5", @"4", @"3", @"2"];
+    //用来保存前17位各自乖以加权因子后的总和
+    NSInteger idCardWiSum = 0;
+    for(int i = 0;i < 17;i++) {
+        NSInteger subStrIndex  = [[identityString substringWithRange:NSMakeRange(i, 1)] integerValue];
+        NSInteger idCardWiIndex = [[idCardWiArray objectAtIndex:i] integerValue];
+        idCardWiSum      += subStrIndex * idCardWiIndex;
+    }
+    //计算出校验码所在数组的位置
+    NSInteger idCardMod=idCardWiSum%11;
+    //得到最后一位身份证号码
+    NSString *idCardLast= [identityString substringWithRange:NSMakeRange(17, 1)];
+    //如果等于2，则说明校验码是10，身份证号码最后一位应该是X
+    if(idCardMod==2) {
+        if(![idCardLast isEqualToString:@"X"]||[idCardLast isEqualToString:@"x"]) {
+            return NO;
+        }
+    }else{
+        //用计算出的验证码与最后一位身份证号码匹配，如果一致，说明通过，否则是无效的身份证号码
+        if(![idCardLast isEqualToString: [idCardYArray objectAtIndex:idCardMod]]) {
+            return NO;
+        }
+    }
+    return YES;
 }
 
 -(NSMutableArray*)pro_applyArr{
